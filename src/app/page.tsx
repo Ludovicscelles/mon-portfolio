a4fe1a468d4c14ac1bec8f4c5283c6fcf8272215
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-05-24 18:36:47 +0200

    responsive design adaptation for the introduction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import ImageWebDev from "../../public/web_dev_desktop.png";
+import ImageWebDevMobile from "../../public/web_dev_mobile.svg";
+import { useEffect, useState } from "react";
 
 export default function IntroPage() {
   const router = useRouter();
@@ -10,13 +12,26 @@ export default function IntroPage() {
   const handleClick = () => {
     router.push("/home");
   };
+
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <div
       onClick={handleClick}
       className="w-screen h-screen cursor-pointer flex justify-center items-center bg-black"
     >
       <Image
-        src={ImageWebDev}
+        src={isMobile ? ImageWebDevMobile : ImageWebDev}
         alt="Entrez sur le site"
         className="w-full h-full object-cover"
         priority
