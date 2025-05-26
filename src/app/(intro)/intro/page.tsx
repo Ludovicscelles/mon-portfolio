e3f378cfc1d6796bd91816a9d7e3c54bc15ba810
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-05-26 19:42:11 +0200

    page rearrangement and framework implementation

---
+++ src/app/(intro)/intro/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import ImageWebDev from "../../../../public/web_dev_desktop.png";
+import ImageWebDevMobile from "../../../../public/web_dev_mobile.svg";
+import { useEffect, useState } from "react";
+
+export default function IntroPage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/home");
+  };
+
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
+  return (
+    <div
+      onClick={handleClick}
+      className="w-screen h-screen cursor-pointer flex justify-center items-center bg-black"
+    >
+      <Image
+        src={isMobile ? ImageWebDevMobile : ImageWebDev}
+        alt="Entrez sur le site"
+        className="w-full h-full object-cover"
+        priority
+      />
+      {isMobile ? (
+        <div className="absolute texte-black text-xl md:text-3xl font-semibold animate-pulse pointer-events-none">
+          Entrez ici
+        </div>
+      ) : (
+        <div className="absolute opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-black text-xl md:text-3xl font-semibold animate-pulse pointer-events-none">
+          Cliquez ici 🖱️
+        </div>
+      )}
+    </div>
+  );
+}
