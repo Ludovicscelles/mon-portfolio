64140c4f51e069ed598f8d03ea48a09a35609d91
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-05-31 19:36:27 +0200

    add data/About.ts file

---
+++ data/About.ts
@@ -0,0 +1,17 @@
+export type About = {
+  subsection1: string;
+  subsection2: string;
+  subsection3: string;
+  subsection4: string;
+};
+
+export const about: About = {
+  subsection1:
+    "Après plusieurs années dans le secteur de l’impression et de la personnalisation, où j'étais en charge des achats et du suivi de production de documents publicitaires, j’ai choisi de me reconvertir dans le développement web fullstack.",
+  subsection2:
+    "Entre février et juillet 2024, j'ai suivi une première formation intensive en développement web et web mobile à la Wild Code School pour laquelle j’ai obtenu la certification RNCP de niveau 5 (BAC +2).",
+  subsection3:
+    "Souhaitant aller plus loin, j’ai poursuivi avec une seconde formation en conception et développement d’applications, de septembre 2024 à janvier 2025, qui m’a permis d’obtenir la certification RNCP de niveau 6 (BAC+3).",
+  subsection4:
+    "Toujours désireux de me perfectionner et d’apprendre davantage, je suis aujourd’hui à la recherche d’opportunités pour contribuer à des projets innovants au sein d’équipes dynamiques.",
+};
