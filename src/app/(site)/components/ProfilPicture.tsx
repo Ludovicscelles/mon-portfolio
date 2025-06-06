b5866e2e8bd92ff5096d1c4720df534aac685d8f
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-06 19:17:24 +0200

    creation component openingToProjects

--- src/app/(site)/components/ProfilPicture.tsx
+++ src/app/(site)/components/ProfilPicture.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export default function ProfilPicture() {
   return (
-    <div className="relative w-40 h-40 md:w-96 md:h-96 ">
+    <div className="relative w-40 h-40 mb-10 md:w-96 md:h-96 md:ml-10 ">
       <div className="absolute  w-40 h-40 bg-purple -right-2 top-2 rounded-3xl md:-right-4 md:top-4 z-0 md:w-96 md:h-96"></div>
-      <div className="relative w-40 h-40 rounded-3xl overflow-hidden z-10 md:w-96 md:h-96">
+      <div className="relative w-40 h-40 border border-[var(--purple)] rounded-3xl overflow-hidden z-10 md:w-96 md:h-96">
         <Image
           src="/images/profil/ludovic2.png"
           alt="Photo de Ludovic Scelles"
--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -1,10 +1,15 @@
 import ProfilPicture from "../components/ProfilPicture";
+import OpeningToProjects from "../components/OpeningToProjects";
 
 export default function Home() {
   return (
     <div>
-      <h1>Ludovic Scelles</h1>
+    <h1>Ludovic Scelles</h1>
+    <div className="inline-block md:flex">
+      
       <ProfilPicture />
+      <OpeningToProjects />
+    </div>
     </div>
   );
 }
