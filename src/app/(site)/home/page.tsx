b00a4e37196e838b25b4c5107873a51a8f2fe9e5
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-07 16:30:27 +0200

    creation component homePage

--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -4,12 +4,13 @@ import OpeningToProjects from "../components/OpeningToProjects";
 export default function Home() {
   return (
     <div>
-    <h1>Ludovic Scelles</h1>
-    <div className="inline-block md:flex">
-      
-      <ProfilPicture />
-      <OpeningToProjects />
-    </div>
+      <h1 className="font-reddit blue-text text-3xl md:text-8xl font-semibold uppercase text-center mt-5 md:mt-10 mb-5 md:mb-10">
+        Ludovic <br className="bloc md:hidden" /> Scelles
+      </h1>
+      <div className="inline-block md:flex">
+        <ProfilPicture />
+        <OpeningToProjects />
+      </div>
     </div>
   );
 }
