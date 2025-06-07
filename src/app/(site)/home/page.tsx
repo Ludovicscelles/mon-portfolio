8b56cfed486f0ded900ac2d11fe10fc9acf87442
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-07 17:59:41 +0200

    Fix homePage code + add links to associated pages ProfilPicture and OpeningToProject components.

--- src/app/(site)/home/page.tsx
+++ src/app/(site)/home/page.tsx
@@ -1,15 +1,21 @@
 import ProfilPicture from "../components/ProfilPicture";
 import OpeningToProjects from "../components/OpeningToProjects";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 className="font-reddit blue-text text-3xl md:text-8xl font-semibold uppercase text-center mt-5 md:mt-10 mb-5 md:mb-10">
-        Ludovic <br className="bloc md:hidden" /> Scelles
+        <span className="block md:inline">Ludovic</span>
+        <span className="block md:inline"> Scelles</span>
       </h1>
       <div className="inline-block md:flex">
-        <ProfilPicture />
+        <Link href="/apropos">
+          <ProfilPicture />
+        </Link>
+        <Link href="/projets">
         <OpeningToProjects />
+        </Link>
       </div>
     </div>
   );
