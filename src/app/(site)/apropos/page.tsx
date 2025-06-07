7dd585ce8e4aeda4277aaed6453695662a1b3cba
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-07 21:03:28 +0200

    creation page about

--- src/app/(site)/apropos/page.tsx
+++ src/app/(site)/apropos/page.tsx
@@ -1,10 +1,16 @@
 import Presentation from "../components/Presentation";
+import ProfilPicture from "../components/ProfilPicture";
 
 export default function About() {
   return (
     <div>
-      <h1>À propos de moi</h1>
-      <Presentation />
+      <h1 className="font-reddit blue-text text-2xl md:text-6xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-20">
+        À propos de moi
+      </h1>
+      <div className="flex flex-col items-center md:flex-row md:items-start">
+        <ProfilPicture size="small" />
+        <Presentation />
+      </div>
     </div>
   );
 }
--- src/app/(site)/components/ProfilPicture.tsx
+++ src/app/(site)/components/ProfilPicture.tsx
@@ -2,11 +2,26 @@
 
 import Image from "next/image";
 
-export default function ProfilPicture() {
+type Props = {
+  size?: "small" | "medium";
+};
+
+export default function ProfilPicture({ size = "medium" }: Props) {
+  const sizes = {
+    small: "w-40 h-40",
+    medium: "w-40 h-40 md:w-96 md:h-96",
+  };
+
+  const sizeClass = sizes[size];
+
   return (
-    <div className="relative w-40 h-40 mb-10 md:w-96 md:h-96 md:ml-10 ">
-      <div className="absolute  w-40 h-40 bg-purple -right-2 top-2 rounded-3xl md:-right-4 md:top-4 z-0 md:w-96 md:h-96"></div>
-      <div className="relative w-40 h-40 border border-[var(--purple)] rounded-3xl overflow-hidden z-10 md:w-96 md:h-96">
+    <div className={`relative ${sizeClass} mb-10 md:ml-10`}>
+      <div
+        className={`absolute bg-purple -right-2 top-2 rounded-3xl z-0 ${size === "small" ? "w-40 h-40 md:-right-2 md:top-2" : "w-40 h-40 md:w-96 md:h-96 md:-right-4 md:top-4"}`}
+      ></div>
+      <div
+        className={`relative border border-[var(--purple)] rounded-3xl overflow-hidden z-10 ${size === "small" ? "w-40 h-40" : "w-40 h-40 md:w-96 md:h-96"} `}
+      >
         <Image
           src="/images/profil/ludovic2.png"
           alt="Photo de Ludovic Scelles"
