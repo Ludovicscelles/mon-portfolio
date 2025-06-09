61f0bd54afc97652106103a4f2d05eff319c9ee5
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-09 15:16:20 +0200

    Fix : activation Reddit font for title

--- src/app/(site)/apropos/page.tsx
+++ src/app/(site)/apropos/page.tsx
@@ -4,7 +4,7 @@ import ProfilPicture from "../components/ProfilPicture";
 export default function About() {
   return (
     <div>
-      <h1 className="font-reddit blue-text text-2xl md:text-6xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-20">
+      <h1 className="title blue-text text-2xl md:text-6xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-20">
         À propos de moi
       </h1>
       <div className="flex flex-col items-center md:flex-row md:items-start">
--- src/app/(site)/competences/page.tsx
+++ src/app/(site)/competences/page.tsx
@@ -5,12 +5,14 @@ import SkillCard from "../components/SkillCard";
 export default function Competences() {
   return (
     <div>
-      <h1 className="font-reddit blue-text text-2xl md:text-6xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-20">
+      <h1 className="title blue-text text-2xl md:text-6xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-20">
         Compétences
       </h1>
       {skills.map((category, index) => (
         <div key={index}>
-          <h2 className="font-reddit blue-text text-2xl md:text-4xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-10">{category.category}</h2>
+          <h2 className="title blue-text text-2xl md:text-4xl font-semibold text-center mt-5 md:mt-30 mb-5 md:mb-10">
+            {category.category}
+          </h2>
           <SkillCard skills={category.skills} />
         </div>
       ))}
